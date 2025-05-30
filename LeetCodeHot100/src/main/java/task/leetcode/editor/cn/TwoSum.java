3a292742fdfb215fbfe844c6d8cf4bbc113e708c
wzc <wangzicen0627@gmail.com>
2025-05-30 13:15:23 +0800

    -init -初始化框架结构 -wzc

---
+++ LeetCodeHot100/src/main/java/task/leetcode/editor/cn/TwoSum.java
@@ -0,0 +1,16 @@
+package task.leetcode.editor.cn;
+  public class TwoSum{
+      public static void main(String[] args) {
+
+          Solution solution = new TwoSum().new Solution();
+
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }

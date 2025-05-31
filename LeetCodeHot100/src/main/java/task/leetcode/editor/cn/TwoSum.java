d693f0a3305099cccfaa2ef287fe287de69e579e
wzc <wangzicen0627@gmail.com>
2025-05-31 16:52:20 +0800

    -code -两数之和 -wzc

--- LeetCodeHot100/src/main/java/task/leetcode/editor/cn/TwoSum.java
+++ LeetCodeHot100/src/main/java/task/leetcode/editor/cn/TwoSum.java
@@ -1,14 +1,25 @@
 package task.leetcode.editor.cn;
-  public class TwoSum{
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum{
       public static void main(String[] args) {
 
           Solution solution = new TwoSum().new Solution();
-
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int curTarget = target - nums[i];
+            if(hashMap.containsKey(curTarget)){
+                return new int[]{i,hashMap.get(curTarget)};
+            }
+           hashMap.put(nums[i],i);
+       }
+        return null;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
